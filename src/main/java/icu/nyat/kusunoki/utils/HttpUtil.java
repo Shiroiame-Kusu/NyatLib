17f9f9f208a414fd0b8ba77923904daa326bed32
Shiroiame-Kusu <kusu@kusu.moe>
2023-08-16 18:37:17 +0800

    Ver ?.?.?

--- src/main/java/icu/nyat/kusunoki/utils/HttpUtil.java
+++ src/main/java/icu/nyat/kusunoki/utils/HttpUtil.java
@@ -5,13 +5,14 @@ import java.io.IOException;
 
 import org.apache.hc.client5.http.fluent.Request;
 import org.apache.hc.client5.http.fluent.Response;
+import org.apache.hc.core5.util.Timeout;
 
 public class HttpUtil {
     NyatLibLogger Logger = new NyatLibLogger();
     public String get(String url) {
         String result = null;
         try {
-            Response response = Request.get(url).execute();
+            Response response = Request.get(url).connectTimeout(Timeout.ofSeconds(3)).responseTimeout(Timeout.ofSeconds(5)).execute();
             result = response.returnContent().asString();
         } catch (IOException e) {
             Logger.logERROR(e.toString());
